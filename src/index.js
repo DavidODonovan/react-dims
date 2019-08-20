741596caba0e76b0c7f3b9275a90f181a103af48
David O'Donovan <d@davidodonovan.com>
2019-08-20 19:59:08 +1000

    delete extraneous file, rename to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { useRef, useState, useEffect } from 'react';
 
-const ReactLand = React.createContext(null);
+const ReactDims = React.createContext(null);
 
 export const Provider = (props)=>{
   const domNode = useRef(null);
   const [dimensions, setDimensions] = useState({});
-	const [invocationContext, setInvocationContext] = useState(0);
+  const [invocationContext, setInvocationContext] = useState(0);
 
   useEffect(()=>{
     setDimensions(domNode.current.getBoundingClientRect());
@@ -19,24 +19,24 @@ export const Provider = (props)=>{
   const getNodeDimensions = ()=>{
     clearTimeout(invocationContext);
     setInvocationContext(()=>setTimeout(()=>{
-	      setDimensions(domNode.current.getBoundingClientRect());
-	    }, 100)
-		);
+      setDimensions(domNode.current.getBoundingClientRect());
+    }, 100)
+    );
   };
 
   return (
-      <div ref={domNode} style={{height: '100%'}}>
-        <ReactLand.Provider value={dimensions}>
-					{props.children}
-        </ReactLand.Provider>
-      </div>
+    <div ref={domNode} style={{height: '100%'}}>
+      <ReactDims.Provider value={dimensions}>
+        {props.children}
+      </ReactDims.Provider>
+    </div>
   )
 };
 
 export const withContext=(ChildComponent)=>{
-	return (props)=>(
-			<ReactLand.Consumer>
-				{(incomingDims)=>(<ChildComponent {...props} dims={incomingDims} />)}
-			</ReactLand.Consumer>
-		)
+  return (props)=>(
+        <ReactDims.Consumer>
+          {(incomingDims)=>(<ChildComponent {...props} dims={incomingDims} />)}
+        </ReactDims.Consumer>
+  )
 };

eaf89f8be190ce9e16327472ecbd2548f3afa5a7
Davido <d@davidodonovan.com>
2021-04-13 16:21:22 +1000

    test for existence of dom node current

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,8 +1,13 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { useReactDims } from 'react-dims';
 const App=()=>{
   // const [domNode, dims] = useReactDims();
   const domNode = useRef(null);
+  useEffect(()=>{
+    if(!domNode.current){
+      console.log("no dom node current")
+    }
+  },[])
   return (
     <div>lkajsd</div>
   );

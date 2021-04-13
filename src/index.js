e3326f1cdabf9a3cfbc5112682f21ef45e6fd092
Davido <d@davidodonovan.com>
2021-04-13 15:41:52 +1000

    change description

--- src/index.js
+++ src/index.js
@@ -49,3 +49,29 @@ export const withContext=(ChildComponent)=>{
         </ReactDims.Consumer>
   )
 };
+
+export const useReactDims=(props)=>{
+  const domNode = useRef(null);
+  const [dimensions, setDimensions] = useState({});
+  const [timeoutID, newTimeoutID] = useState(null);
+
+  useEffect(()=>{
+    setDimensions(domNode.current.getBoundingClientRect());
+  },[]);
+
+  useEffect(()=>{
+    const getNodeDimensions=()=>{
+      clearTimeout(timeoutID);
+      newTimeoutID(setTimeout(()=>{
+          setDimensions(domNode.current.getBoundingClientRect());
+        }, 100)
+      );
+    };
+    window.addEventListener('resize', getNodeDimensions);
+    return ()=>{ window.removeEventListener('resize', getNodeDimensions)};
+  }, [timeoutID]);
+
+  return (
+    [domNode, dimensions]
+  );
+};
--- useReactDims/useReactDims.js
+++ useReactDims/useReactDims.js
@@ -1,8 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 
-// TODO: test then publish breaking changes and new docs to npm
-
-const useReactDims = (props)=>{
+const useReactDims=(props)=>{
   const domNode = useRef(null);
   const [dimensions, setDimensions] = useState({});
   const [timeoutID, newTimeoutID] = useState(null);
